2a0baef125a08193274293d6e7be68e49b6b5422
Yordan Shterev <shterew@gmail.com>
2023-01-25 17:46:23 +0200

    Updates

--- static/js/react-builder.js
+++ static/js/react-builder.js
@@ -1,5 +1,5 @@
 (function () {
-  var reactFiles = 'static/js/main.3b174a48.js';
+  var reactFiles = 'static/js/main.0e111b68.js';
   var reactFiles = reactFiles.split(',');
   var reactFilesSize = reactFiles.length;
   var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
