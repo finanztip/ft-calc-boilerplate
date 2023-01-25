812bcf8bde1b9cdd474b06320225f341703a0d62
Yordan Shterev <shterew@gmail.com>
2023-01-25 17:38:20 +0200

    Updates

---
+++ static/js/TestRootCalculator.377b1ee4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkft_calc_boilerplate=self.webpackChunkft_calc_boilerplate||[]).push([[275],{886:function(e,t,c){c.r(t),c.d(t,{default:function(){return l}});var n=c(184);function l(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("p",{children:"Click the button to see the secret message"}),(0,n.jsx)("button",{onClick:function(){alert("You just clicked me!")},children:"Click me"})]})}}}]);
+//# sourceMappingURL=TestRootCalculator.377b1ee4.chunk.js.map
