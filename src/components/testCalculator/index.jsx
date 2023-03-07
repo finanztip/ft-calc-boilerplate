a05ba233eee712ab79f59398b812d1f928e7c4b2
Yordan Shterev <shterew@gmail.com>
2023-03-07 13:53:55 +0200

    Add test component coming from app-components

--- src/components/testCalculator/index.jsx
+++ src/components/testCalculator/index.jsx
@@ -1,15 +1,13 @@
 import  { useState } from 'react';
-
+import {TextButton, Paragraph} from '@finanztip/app-components'
 
 export default function TestCalculator() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-    <p>You clicked {count} times.</p>
-    <button onClick={() => setCount(count + 1)}>
-      Click me
-    </button>
-  </div>
+      <Paragraph content={`You clicked ${count} times.`} />
+      <TextButton title="Click me to count!" onPress={() => setCount(count + 1)}/>
+    </div>
   );
 }
